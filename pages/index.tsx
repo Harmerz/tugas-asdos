50ee5c712f874950bd6fe7583320a59a2f52d567
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-10-27 17:22:51 +0700

    feat : react icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { gql, useQuery } from '@apollo/client'
 import { io } from 'socket.io-client'
+import { IoLogOutOutline } from 'react-icons/io5'
 
 const ViewerQuery = gql`
   query ViewerQuery {
@@ -32,9 +33,17 @@ const Index = () => {
 
   if (viewer) {
     return (
-      <div>
-        You're signed in as {viewer.username}. Go to <Link href="/about">about</Link> page or{' '}
-        <Link href="/signout">signout</Link>.
+      <div className="flex h-screen flex-col items-center justify-center">
+        <div className="h-12 w-full text-center text-xl">
+          You're signed in as <span className="font-bold text-blue-500">{viewer.username}</span>
+        </div>
+        <button
+          key="Logout"
+          type="button"
+          className=" flex w-[90px] flex-row items-center gap-2 text-red-600 hover:text-red-400"
+        >
+          <IoLogOutOutline className="h-5 w-5 text-xl" /> Log Out
+        </button>
       </div>
     )
   }
